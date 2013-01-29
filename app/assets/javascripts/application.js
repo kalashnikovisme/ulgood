d72d5cb2470f48d64ffdee330bf76b4839c9ced6
Павел Калашников <kalashnikovisme@gmail.com>
2013-01-30 01:57:22 +0400

    removed allmost all html tags in haml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,23 @@ function show_full(el) {
         el.style.wordWrap = "";
     };
 }
+
+function applicationFunc() {
+    var reformalOptions = {
+    project_id: 89773,
+    project_host: "ulgood.reformal.ru",
+    force_new_window: true,
+    tab_orientation: "right",
+    tab_indent: "50%",
+    tab_bg_color: "#f9be27",
+    tab_border_color: "#FFFFFF",
+    tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/a08a7c60392f68cb33f77d4f56cf8c6f/right/1/tab.png",
+    tab_border_width: 0
+    };
+    (function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript'; script.async = true;
+    script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+    })();
+}
